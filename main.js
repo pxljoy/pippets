6ffc5c760e8e3c1bcac00d9cac562524da3b82d1
Pim <pj@pxljoy.io>
2020-01-03 17:18:17 +1030

    icons / launching

--- main.js
+++ main.js
@@ -1,152 +1,29 @@
 const electron = require('electron');
 const path = require('path');
-const uniq = require('lodash/uniq');
+const fs = require('fs');
 
 const Store = require('electron-store');
+
 const store = new Store();
-
-
-// const getAnglePO = (x, y) => {
-//   if (x > 0) {
-//     if (y >= 0) {
-//       return Math.atan(y / x);
-//     }
-//     return Math.atan(y / x) + 2 * Math.PI;
-//   }
-//   if (!x) {
-//     if (!y) {
-//       return 0;
-//     }
-//     if (y > 0) {
-//       return Math.PI / 2;
-//     }
-//     return 3 * Math.PI / 2;
-//   }
-//   return Math.atan(y / x) + Math.PI;
-// }
-
 
 // const debug = require('electron-debug');
 
 // debug();
 
-let tray = undefined
-let window = undefined
+let tray;
+let window;
 
-const {app, BrowserWindow, ipcMain, Tray, Menu} = electron;
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Tray,
+  shell,
+} = electron;
 
 // Don't show the app in the doc
-app.dock.hide()
+app.dock.hide();
 
-app.on('ready', () => {
-  createTray()
-  createWindow()
-
-  // let oldMX;
-  // let oldMY;
-
-  // let curMX;
-  // let curMY;
-
-  // let current = '';
-
-  // // const gesture1 = '46464';
-  // // const gesture2 = '19191';
-  // // const gesture3 = '73737';
-  // // const gesture4 = '28282';
-  // // const gesture5 = '64646';
-  // // const gesture6 = '91919';
-  // // const gesture7 = '37373';
-  // // const gesture8 = '82828';
-
-  // let trig = 0;
-  // let shown = false;
-
-  // setInterval(function () {
-  //   trig += 1;
-  //   if (!shown) {
-  //     const { x, y } = electron.screen.getCursorScreenPoint();
-  //     oldMX = curMX;
-  //     oldMY = curMY;
-
-  //     curMX = x;
-  //     curMY = y;
-
-  //     distX = Math.abs(curMX - oldMX) * Math.abs(curMX - oldMX)
-  //     distY = Math.abs(curMY - oldMY) * Math.abs(curMY - oldMY)
-  //     curDist = Math.round(Math.sqrt(distX + distY))
-
-  //     if (curDist > 50) {
-  //       const a = getAnglePO(curMY - oldMY, curMX - oldMX) * 57.2957795130823;
-  //       let direction;
-  //       if (a >= 337.5 || a < 22.5) {
-  //         direction = '6';
-  //       } else if (a >= 22.5 && a < 67.5) {
-  //         direction = '3';
-  //       } else if (a >= 67.5 && a < 112.5) {
-  //         direction = '2';
-  //       } else if (a >= 112.5 && a < 157.5) {
-  //         direction = '1';
-  //       } else if (a >= 157.5 && a < 202.5) {
-  //         direction = '4';
-  //       } else if (a >= 202.5 && a < 247.5) {
-  //         direction = '7';
-  //       } else if (a >= 247.5 && a < 292.5) {
-  //         direction = '8';
-  //       } else if (a >= 292.5 && a < 337.5) {
-  //         direction = '9';
-  //       }
-
-  //       if (direction !== current[current.length - 1]) {
-  //         current += direction;
-  //       }
-  //     }
-  //     if (trig === 100) {
-  //       // console.log(current);
-  //       if (current.length >= 3) {
-  //         current = '';
-  //         // console.log('SHOOK');
-
-  //         window.setPosition(x, y, false);
-
-  //         window.setVisibleOnAllWorkspaces(true); // put the window on all screens
-  //         window.focus(); // focus the window up front on the active screen
-  //         window.setVisibleOnAllWorkspaces(false); // disable all screen behavior
-
-  //         window.show();
-  //       }
-
-  //       current = '';
-  //     }
-  //   }
-  //   if (trig === 100) trig = 0;
-
-  // }, 1);
-
-})
-
-const createTray = () => {
-  tray = new Tray(path.join(__dirname, './src/assets/sunTemplate.png'));
-  tray.setToolTip('Drag files to hold');
-  tray.setTitle(`${store.get('files').length || ''}`);
-  tray.on('drop-files', (_, files) => {
-    store.set('files', files);
-    tray.setToolTip('Click to open palette');
-    window.webContents.send('receive-store', store.store);
-    showWindow();
-    tray.setTitle(store.get('files').length + '');
-  });
-  tray.on('right-click', () => {
-    tray.setTitle('');
-    store.set('files', []);
-    tray.setToolTip('Drag files into icon to hold');
-    window.webContents.send('receive-store', store.store);
-  });
-  tray.on('click', () => {
-    showWindow();
-  });
-
-}
 
 const getWindowPosition = () => {
   const windowBounds = window.getBounds();
@@ -158,52 +35,9 @@ const getWindowPosition = () => {
   // Position window 4 pixels vertically below the tray icon
   const y = Math.round(trayBounds.y + trayBounds.height + 4);
 
-  return { x: x, y: y };
-}
+  return { x, y };
+};
 
-const createWindow = () => {
-  window = new BrowserWindow({
-    width: 200,
-    height: 300,
-    show: false,
-    frame: false,
-    fullscreenable: false,
-    resizable: false,
-    transparent: true,
-    webPreferences: {
-      backgroundThrottling: false,
-      nodeIntegration: true
-    },
-    name: 'Pippets',
-    vibrancy: 'dark',
-    acceptFirstMouse: true,
-    backgroundColor: '#AA000000',
-  })
-
-  window.loadURL(`file://${path.join(__dirname, 'public/index.html')}`)
-
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
-    }
-  })
-
-  window.webContents.on('did-finish-load', () => {
-    window.webContents.send('receive-store', store.store);
-  });
-
-  ipcMain.on('drag-file', (_, file) => {
-    window.webContents.startDrag({
-      file,
-      icon: path.join(__dirname, './src/assets/img.png')
-    });
-  });
-}
-
-const toggleWindow = () => {
-  window.isVisible() ? window.hide() : showWindow();
-}
 
 const showWindow = () => {
   const position = getWindowPosition();
@@ -214,8 +48,103 @@ const showWindow = () => {
   window.setVisibleOnAllWorkspaces(false); // disable all screen behavior
 
   window.show();
-}
+};
+
+const toggleWindow = () => {
+  if (window.isVisible()) {
+    window.hide();
+  } else {
+    showWindow();
+  }
+};
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 400,
+    height: 600,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    webPreferences: {
+      backgroundThrottling: false,
+      nodeIntegration: true,
+    },
+    name: 'Pippets',
+    vibrancy: 'dark',
+    acceptFirstMouse: true,
+    backgroundColor: '#AA000000',
+  });
+
+  window.loadURL(`file://${path.join(__dirname, 'public/index.html')}`);
+
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide();
+    }
+  });
+
+  window.webContents.on('did-finish-load', () => {
+    window.webContents.send('receive-store', store.store);
+  });
+
+  ipcMain.on('drag-file', (_, file) => {
+    window.webContents.startDrag({
+      file,
+      icon: path.join(__dirname, './src/assets/img.png'),
+    });
+  });
+};
+
+const addFiles = async (files) => {
+  const images = await Promise.all(files.map(async (file) => ({
+    path: file,
+    ext: path.extname(file).substr(1),
+    directory: fs.lstatSync(file).isDirectory(),
+    image: await (await app.getFileIcon(file, { size: 'normal' })).toDataURL(),
+  })));
+
+  store.set('files', images);
+  tray.setTitle(`${images.length}`);
+  tray.setToolTip('Click to open palette');
+  window.webContents.send('receive-store', store.store);
+};
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, './src/assets/sunTemplate.png'));
+  tray.setToolTip('Drag files to hold');
+  tray.setTitle(`${store.get('files').length || ''}`);
+  tray.on('drop-files', async (_, files) => {
+    await addFiles(files);
+    showWindow();
+  });
+  tray.on('right-click', () => {
+    tray.setTitle('');
+    store.set('files', []);
+    tray.setToolTip('Drag files into icon to hold');
+    window.webContents.send('receive-store', store.store);
+    window.hide();
+  });
+  tray.on('click', () => {
+    toggleWindow();
+  });
+};
+
+app.on('ready', () => {
+  createTray();
+  createWindow();
+});
+
+app.on('open-file', (_, file) => {
+  addFiles([file]);
+});
+
 
 ipcMain.on('show-window', () => {
   showWindow();
-})
+});
+
+ipcMain.on('open-file', (_, file) => {
+  shell.openItem(file);
+});
--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,24 @@
 
 	bus.on('receive-store', (event, message) => {
 		store = message;
+		console.log(store);
 	});
 
 	const open = (e, file) => {
 		e.preventDefault();
 		bus.send('drag-file', file);
 	}
+	const launch = (e, file) => {
+		e.preventDefault();
+		bus.send('open-file', file);
+	}
+	const renderableExts = {
+		png: true,
+		jpg: true,
+		jpeg: true,
+		bmp: true,
+		svg: true,
+	};
 </script>
 
 <style global lang="scss" src="./theme/index.scss"></style>
@@ -17,6 +29,24 @@
 <div class="pa-4">
 <p style="font-size: 10px; opacity: 0.7;" class="mb-4">Drag files into the icon to add and right click the icon to clear.</p>
 	{#each (store.files || []) as file}
-	<button style="width: 100%;" draggable="true" on:dragstart={(e) => open(e, file)}>{file.split('/').slice(-1)}</button>
+	<button
+		class="mb-2"
+		style="width: 100%; display: flex; justify-content: start; align-items: center;"
+		draggable="true"
+		on:dragstart={(e) => open(e, file.path)}
+		on:click={(e) => launch(e, file.path)}
+	>
+		{#if file.directory}
+			<img width="16" alt="image" src="../src/assets/folder.png">
+		{:else if renderableExts[file.ext]}
+			<img width="16" alt="image" src={file.path}>
+		{:else if file.image}
+			<img width="16" alt="image" src={file.image}>
+		{/if}
+		<span>
+			&nbsp;
+			{file.path.split('/').slice(-1)}
+		</span>
+	</button>
 	{/each}
 </div>
